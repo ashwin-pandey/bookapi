1898c79fb8d963dfa60760f73ec76a7ddcb39607
ashwin-pandey <ashwin.pandey11197@gmail.com>
2019-09-20 18:07:16 +0530

    save book to database

--- src/main/java/com/ashwin/spring/controller/BookController.java
+++ src/main/java/com/ashwin/spring/controller/BookController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ashwin.spring.model.Book;
@@ -17,10 +19,17 @@ public class BookController {
 	private BookService bookService;
 	
 	// Get all the books
-	@GetMapping("/api/books")
+	@GetMapping("/api/book")
 	public ResponseEntity<List<Book>> list() {
 		List<Book> list = bookService.list();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	// Save the book
+	@PostMapping("/api/book")
+	public ResponseEntity<?> save(@RequestBody Book book) {
+		long id = bookService.save(book);
+		return ResponseEntity.ok().body("Book created with id: " + id);
 	}
 	
 }
